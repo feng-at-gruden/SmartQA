d33f8226147b8b95d5cffb2e04d173fcb30d070a
Feng Guo <feng@gruden.com>
2017-06-19 22:19:58 +0800

    init git

---
+++ SmartCMS/Controllers/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartCMS.Controllers
+{
+    public class AdminController : Controller
+    {
+        
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult Categories()
+        {
+            return View();
+        }
+
+
+        public ActionResult AddCategory(int pid)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(int pid)
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ SmartCMS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SmartCMS.MvcApplication" Language="C#" %>
